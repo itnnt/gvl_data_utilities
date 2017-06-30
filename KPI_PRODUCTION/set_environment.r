847b534561f1e46d555cf4022571cddf35bc27a7
itnnt <tung.nn83@gmail.com>
2017-06-30 18:33:55 +0700

    update code

---
+++ KPI_PRODUCTION/set_environment.r
@@ -0,0 +1,25 @@
+# *** ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Required libraries ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+library(dplyr)
+library(RSQLite)
+library(xlsx)
+library (lubridate)
+library(zoo)
+
+Sys.setenv(
+  # Define common format string
+  KPI_PRODUCTION_FORMAT_COMMONDATE = '%d/%m/%Y',
+  KPI_PRODUCTION_FORMAT_BUSSINESSDATE = '%Y-%m-%d',
+  KPI_PRODUCTION_FORMAT_MONTH_YEAR_y_b = '%b-%y',
+  KPI_PRODUCTION_FORMAT_YEAR_y = '%y'
+)
+
+# *** ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Define common format string ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+FORMAT_COMMONDATE     = Sys.getenv("KPI_PRODUCTION_FORMAT_COMMONDATE")
+FORMAT_BUSSINESSDATE  = Sys.getenv("KPI_PRODUCTION_FORMAT_BUSSINESSDATE")
+FORMAT_MONTH_YEAR_y_b = Sys.getenv("KPI_PRODUCTION_FORMAT_MONTH_YEAR_y_b")
+FORMAT_YEAR_y         = Sys.getenv("KPI_PRODUCTION_FORMAT_YEAR_y")
+
+source('KPI_PRODUCTION/Common functions.R')
