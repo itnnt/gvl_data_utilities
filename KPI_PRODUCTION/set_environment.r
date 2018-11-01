b7018b7b2c7f1de603436aac7cb918c861bc55d3
admin <tung.nn83@gmail.com>
2018-11-01 15:44:19 +0700

    update

--- KPI_PRODUCTION/set_environment.r
+++ KPI_PRODUCTION/set_environment.r
@@ -11,19 +11,19 @@
 # library(zoo)
 
 Sys.setenv(
-  # Define common format string
-  KPI_PRODUCTION_FORMAT_COMMONDATE = '%d/%m/%Y',
-  KPI_PRODUCTION_FORMAT_BUSSINESSDATE = '%Y-%m-%d',
-  KPI_PRODUCTION_FORMAT_MONTH_YEAR_y_b = '%b-%y',
-  KPI_PRODUCTION_FORMAT_YEAR_y = '%y'
+# Define common format string
+KPI_PRODUCTION_FORMAT_COMMONDATE = '%d/%m/%Y',
+KPI_PRODUCTION_FORMAT_BUSSINESSDATE = '%Y-%m-%d',
+KPI_PRODUCTION_FORMAT_MONTH_YEAR_y_b = '%b-%y',
+KPI_PRODUCTION_FORMAT_YEAR_y = '%y'
 )
 
 # *** ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Define common format string ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
-FORMAT_COMMONDATE     = Sys.getenv("KPI_PRODUCTION_FORMAT_COMMONDATE")
-FORMAT_BUSSINESSDATE  = Sys.getenv("KPI_PRODUCTION_FORMAT_BUSSINESSDATE")
+FORMAT_COMMONDATE = Sys.getenv("KPI_PRODUCTION_FORMAT_COMMONDATE")
+FORMAT_BUSSINESSDATE = Sys.getenv("KPI_PRODUCTION_FORMAT_BUSSINESSDATE")
 FORMAT_MONTH_YEAR_y_b = Sys.getenv("KPI_PRODUCTION_FORMAT_MONTH_YEAR_y_b")
-FORMAT_YEAR_y         = Sys.getenv("KPI_PRODUCTION_FORMAT_YEAR_y")
+FORMAT_YEAR_y = Sys.getenv("KPI_PRODUCTION_FORMAT_YEAR_y")
 
 source('KPI_PRODUCTION/Common functions.R')
